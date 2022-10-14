59bc8fb3d2b0dbaa2ba7121643b1952fac68d812
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-14 14:22:47 -0400

    init

--- main.cs
+++ main.cs
@@ -12,15 +12,14 @@ class Program
         // This function accepts user input
         int number;
         string name;
-        
+
         Console.WriteLine("This program gets a user's street number and name.");
         Console.WriteLine("");
-
+        
         Console.Write("Enter your street number: ");
         number = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter your street name: ");
         name = Console.ReadLine();
-        
 
         Console.WriteLine("");
         Console.WriteLine("Your address is: " + number + " " + name + ".");
